5d06b3a384e2325269e0ab4e3da38f97a52fdb98
homebake <oleg.kalyugin2001@mail.ru>
2022-08-30 15:38:54 +0200

    Тени на селектор языка

--- src/component/UI/Select/Select.jsx
+++ src/component/UI/Select/Select.jsx
@@ -2,25 +2,28 @@ import React, {useState} from 'react';
 import classes from './Select.module.css'
 import useToggle from "../../../hooks/useToggle";
 
-const Select = ({selectedItem, setSelectedItem,menuItems}) => {
+const Select = ({selectedItem, setSelectedItem, menuItems, className}) => {
     const [isActive, toggleActive] = useToggle(false)
 
     return (
-        <div className={classes.dropdown}>
+        <div
+            className={classes.dropdown + ' ' + className}
+            style={isActive ? {boxShadow: "0 0 4px #FFFFFF"} : {}}
+            onMouseLeave={ () => isActive && toggleActive()}
+        >
             <div className={classes.dropDownItem} onClick={() => {
                 toggleActive()
             }}>
                 {selectedItem}
             </div>
             {
-
                 isActive && menuItems.map((item,key)=> {
                     if (item !== selectedItem) {
                         return <div
                             className={classes.dropDownItem}
                             style={{borderTop: '1px solid #016DA6'}}
                             key={key}
-                            onClick={(e)=> {
+                            onClick={ ()=> {
                                 setSelectedItem(item)
                                 toggleActive()
                             }
